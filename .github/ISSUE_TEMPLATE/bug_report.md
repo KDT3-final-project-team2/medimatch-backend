95fcde22a110f0954192b13d8201946389535154
HongGuma <hongsu9849@gmail.com>
2023-03-03 20:20:47 +0900

    Initial setting

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,17 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+## 🐞 버그 발생
+어떤 버그인지에 대해서 명확하고 간결하게 설명
+
+## 🚫 발생 위치
+버그가 발생한 위치 (co
+
+## 💡 해결 및 참고 사항
+스크린샷 이나 레퍼런스, 공유할 내용, 추가로 발생할 것으로 예상되는 이슈 등
---
+++ src/main/java/com/project/finalproject/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectApplication.class, args);
+    }
+
+}
