2bdf8d80c9dc1a7485f51a18da4baf7baddc17bf
ThomasChoi <724thomas@gmail.com>
2023-03-23 19:04:11 +0900

    Feat:/#23 Applicant info update

---
+++ src/main/java/com/project/finalproject/applicant/service/ApplicantInfoUpdateService.java
@@ -0,0 +1,8 @@
+package com.project.finalproject.applicant.service;
+
+import com.project.finalproject.applicant.entity.Applicant;
+
+public interface ApplicantInfoUpdateService {
+
+    public String updateApplicantInfo(Applicant applicant);
+}
---
+++ src/main/java/com/project/finalproject/applicant/service/impl/ApplicantInfoUpdateServiceImpl.java
@@ -0,0 +1,19 @@
+package com.project.finalproject.applicant.service.impl;
+
+
+import com.project.finalproject.applicant.entity.Applicant;
+import com.project.finalproject.applicant.repository.ApplicantInfoUpdateRepository;
+import com.project.finalproject.applicant.service.ApplicantInfoUpdateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApplicantInfoUpdateServiceImpl implements ApplicantInfoUpdateService {
+
+    private final ApplicantInfoUpdateRepository applicantInfoUpdateRepository;
+
+    public String updateApplicantInfo(Applicant applicant) {
+        return applicantInfoUpdateRepository.updateApplicantInfo(applicant);
+    }
+}
