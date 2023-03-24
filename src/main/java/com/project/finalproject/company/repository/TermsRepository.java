d37939b648548a025f356a6c6ecb21796782ceef
yunhuiGwak <kwakyh17@naver.com>
2023-03-24 23:15:23 +0900

    feat: 약관 repository setting

---
+++ src/main/java/com/project/finalproject/company/repository/TermsRepository.java
@@ -0,0 +1,7 @@
+package com.project.finalproject.company.repository;
+
+import com.project.finalproject.company.entity.Terms;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TermsRepository extends JpaRepository<Terms,Long> {
+}
