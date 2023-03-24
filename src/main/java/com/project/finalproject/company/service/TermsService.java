5c2f70b3c6f1b2c28de605fa3cbd8b9c6765f731
yunhuiGwak <kwakyh17@naver.com>
2023-03-24 23:16:47 +0900

    feat: 약관 등록 기능구현 (service)

---
+++ src/main/java/com/project/finalproject/company/service/TermsService.java
@@ -0,0 +1,8 @@
+package com.project.finalproject.company.service;
+
+import com.project.finalproject.company.dto.TermsDTO;
+
+public interface TermsService {
+
+    Long register(TermsDTO termsDTO);
+}
