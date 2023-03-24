6f4175b15f5e6371ec251274088591f610010d66
yunhuiGwak <kwakyh17@naver.com>
2023-03-24 23:18:32 +0900

    feat: 약관종류(서비스/개인정보/마케팅) enum 설정

--- src/main/java/com/project/finalproject/company/entity/enums/TermsType.java
+++ src/main/java/com/project/finalproject/company/entity/enums/TermsType.java
@@ -1,5 +1,15 @@
 package com.project.finalproject.company.entity.enums;
 
 public enum TermsType {
+    SERVICE("서비스이용약관"), PRIVACY("개인정보처리방침"), THIRD_PARTY("제3자정보제공"), MARKETING("개인정보마케팅이용");
 
+    private String types;
+
+    TermsType(String types) {
+        this.types = types;
+    }
+
+    public String getTypes() {
+        return types;
+    }
 }
