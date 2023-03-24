5b16c841a02e43e587b8b73eccfadadbc57231e5
HongGuma <hongsu9849@gmail.com>
2023-03-24 20:45:56 +0900

    #18 feat : 개인 회원 채용 공고 전체 리스트 출력 기능 구현

--- src/main/java/com/project/finalproject/jobpost/dto/JobpostResponseDTO.java
+++ src/main/java/com/project/finalproject/jobpost/dto/JobpostResponseDTO.java
@@ -1,4 +1,51 @@
 package com.project.finalproject.jobpost.dto;
 
+import com.project.finalproject.jobpost.entity.Jobpost;
+import com.project.finalproject.jobpost.entity.enums.JobpostStatus;
+import lombok.*;
+
+import java.time.LocalDate;
+
 public class JobpostResponseDTO {
+
+    //리스트 조회시 출력하는 DTO (간략한 정보만 출력)
+    @Getter
+    @Setter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ShortDTO{
+
+        Long postId;
+
+        String title;
+
+        LocalDate startDate;
+
+        LocalDate finishDate;
+
+        int views;
+
+        String companyName;
+
+        JobpostStatus jobpostStatus;
+
+        String contents;
+
+        public ShortDTO(Jobpost jobpost){
+            this.postId = jobpost.getJobpostId();
+            this.title = jobpost.getTitle();
+            this.startDate = jobpost.getStartDate();
+            this.finishDate = jobpost.getFinishDate();
+            this.views = jobpost.getViews();
+            this.companyName = jobpost.getCompany().getName();
+            this.jobpostStatus = jobpost.getJobpostStatus();
+            this.contents = jobpost.getContentsFilePath();
+        }
+    }
+
+    //상세 조회시 출력하는 DTO (모든 정보 출력)
+    class LongDTO{
+
+    }
 }
--- src/main/java/com/project/finalproject/jobpost/service/JobpostService.java
+++ src/main/java/com/project/finalproject/jobpost/service/JobpostService.java
@@ -1,5 +1,13 @@
 package com.project.finalproject.jobpost.service;
 
+import com.project.finalproject.jobpost.dto.JobpostResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface JobpostService {
+
+    List<JobpostResponseDTO.ShortDTO> showJobpostList();
 
 }
