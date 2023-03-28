6082b9e525e0ab90cf16491ff8c4f4c642feb959
724thomas <724thomas@gmail.com>
2023-03-29 01:52:53 +0900

    Feat:/#43 Applicant Signup

---
+++ src/main/java/com/project/finalproject/applicant/service/ApplicantService.java
@@ -0,0 +1,8 @@
+package com.project.finalproject.applicant.service;
+
+import com.project.finalproject.applicant.dto.request.SignupRequestDTO;
+
+public interface ApplicantService {
+
+    public String signup(SignupRequestDTO signupRequestDTO);
+}
