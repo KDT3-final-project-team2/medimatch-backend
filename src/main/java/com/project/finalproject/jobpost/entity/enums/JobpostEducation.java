062bc004e1b75f73e0f1f6534b1e7af7fe48abc3
ThomasChoi <724thomas@gmail.com>
2023-03-30 19:17:24 +0900

    Fix : Changed Jobpost Entity

--- src/main/java/com/project/finalproject/jobpost/entity/enums/JobpostEducation.java
+++ src/main/java/com/project/finalproject/jobpost/entity/enums/JobpostEducation.java
@@ -2,9 +2,9 @@ package com.project.finalproject.jobpost.entity.enums;
 
 public enum JobpostEducation {
     HIGH_SCHOOL("고졸"),
-    COMMUNITY_COLLEGE("초대졸"),
+    JUNIOR_COLLEGE("초대졸"),
     UNIVERSITY("대졸"),
-    GRAD_SCHOOL("석박사");
+    MASTER_AND_DOCTOR("석박사");
 
     String education;
 
