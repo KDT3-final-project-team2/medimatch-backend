6fffa3d42768cfbdc0e4e3ba3579daaaac699c84
HongGuma <hongsu9849@gmail.com>
2023-03-31 01:50:01 +0900

    #19 feat : Create CompanyException

---
+++ src/main/java/com/project/finalproject/company/exception/CompanyExceptionType.java
@@ -0,0 +1,26 @@
+package com.project.finalproject.company.exception;
+
+import com.project.finalproject.global.exception.base.CustomExceptionType;
+
+public enum CompanyExceptionType implements CustomExceptionType {
+    NOT_FOUND_USER(-201, "존재하지 않는 사용자 입니다.")
+    ;
+
+    private int errorCode;
+    private String errorMsg;
+
+    CompanyExceptionType(int errorCode, String errorMsg){
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.getMessage();
+    }
+
+    @Override
+    public int getErrorCode() {
+        return this.getErrorCode();
+    }
+}
