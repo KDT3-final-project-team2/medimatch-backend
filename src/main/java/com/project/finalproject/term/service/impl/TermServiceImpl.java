cb8472081804c4663c66c3e0ee78549b8d5e2b3d
yunhuiGwak <kwakyh17@naver.com>
2023-03-31 21:50:36 +0900

    fix: 약관 등록 serviceImpl 생성

--- src/main/java/com/project/finalproject/term/service/impl/TermServiceImpl.java
+++ src/main/java/com/project/finalproject/term/service/impl/TermServiceImpl.java
@@ -1,37 +1,38 @@
-package com.project.finalproject.term.service;
+package com.project.finalproject.term.service.impl;
 
 import com.project.finalproject.company.entity.Company;
 import com.project.finalproject.company.repository.CompanyRepository;
-import com.project.finalproject.term.dto.TermFormDTO;
+import com.project.finalproject.term.dto.TermResDTO;
 import com.project.finalproject.term.entity.Term;
 import com.project.finalproject.term.repository.TermRepository;
+import com.project.finalproject.term.service.TermService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-
 @Service
 @RequiredArgsConstructor
-@Log4j2
-public class TermServiceImpl implements TermService{
+public class TermServiceImpl implements TermService {
 
     private final TermRepository termRepository;
     private final CompanyRepository companyRepository;
 
     /**
-     * 약관 등록
+     * 슈퍼관리자(admin) 약관 등록
+     * @param termResDTO
+     * @param adminEmail
+     * @return termId
      */
     @Override
-    public Long register(TermFormDTO termFormDTO, String adminEmail) {
+    public Long register(TermResDTO termResDTO, String adminEmail) {
 
         Optional<Company> company = companyRepository.findByEmail(adminEmail);
 
         if (company.isEmpty()) {
             return null;
         } else {
-            Term term = Term.createTerm(termFormDTO,company.get());
+            Term term = Term.createTerm(termResDTO,company.get());
             termRepository.save(term);
 
             return term.getId();
