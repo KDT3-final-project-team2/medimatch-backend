f7e278a80fc7930f6c384c92620ae985de08fc92
HongGuma <hongsu9849@gmail.com>
2023-03-31 15:52:27 +0900

    #19 feat : Add custom exception handler

--- src/main/java/com/project/finalproject/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/project/finalproject/global/exception/CustomExceptionHandler.java
@@ -4,6 +4,7 @@ import com.project.finalproject.company.exception.CompanyException;
 import com.project.finalproject.company.exception.CompanyExceptionType;
 import com.project.finalproject.global.dto.ErrorDTO;
 import com.project.finalproject.global.exception.base.CustomException;
+import com.project.finalproject.jobpost.exception.JobpostException;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,14 +16,13 @@ public class CustomExceptionHandler implements ErrorController {
     public ErrorDTO handleGlobalException(CustomException ce){
         return ErrorDTO.builder()
                 .errorCode(ce.getExceptionType().getErrorCode())
-
                 .errorMessage(ce.getExceptionType().getMessage())
                 .build();
     }
 
-    //Company exception handler
-    @ExceptionHandler(value = CompanyException.class)
-    public ErrorDTO handleCompanyException(CompanyException ce){
+    //Custom exception handler
+    @ExceptionHandler(value = {CompanyException.class, JobpostException.class})
+    public ErrorDTO handleCustomException(CustomException ce){
         return ErrorDTO.builder()
                 .errorCode(ce.getExceptionType().getErrorCode())
                 .errorMessage(ce.getExceptionType().getMessage())
