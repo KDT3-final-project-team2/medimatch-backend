eba4140584a112e9af14b6e531729e1b17d740a8
724thomas <724thomas@gmail.com>
2023-04-02 07:37:04 +0900

    Test : Applicant MyInfoUpdate Test

---
+++ src/test/java/com/project/finalproject/applicant/controller/내_정보수정.java
@@ -0,0 +1,166 @@
+package com.project.finalproject.applicant.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.finalproject.applicant.dto.request.InfoUpdateRequestDTO;
+import com.project.finalproject.applicant.service.ApplicantService;
+import com.project.finalproject.global.dto.ResponseDTO;
+import com.project.finalproject.global.jwt.utils.JwtFilter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc(addFilters = false) //SecurityConfig를 무시하고 테스트
+@WebMvcTest(ApplicantController.class)
+public class 내_정보수정 {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean // ApplicantController에서 사용하는 ApplicantService를 MockBean으로 등록
+    ApplicantService applicantService;
+
+    @MockBean
+    JwtFilter jwtFilter;
+
+
+    @Test
+    @DisplayName("내 정보수정 성공")
+    @WithMockUser
+    public void infoUpdateSuccess() throws Exception {
+
+        given(applicantService.infoUpdate(any(InfoUpdateRequestDTO.class)))
+                .willReturn("success"); // given : Mock 객체가 특정 상황에서 해야하는 행위를 정의하는 메소드.
+
+        MvcResult mvcResult = mockMvc.perform(put("/applicant/me")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new InfoUpdateRequestDTO())))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        String responseContent = mvcResult.getResponse().getContentAsString();
+        ResponseDTO responseDTO = objectMapper.readValue(responseContent, ResponseDTO.class);
+        assertEquals(200, responseDTO.getStateCode());
+        assertEquals(true, responseDTO.isSuccess());
+        assertEquals("success", responseDTO.getData());
+
+        verify(applicantService).infoUpdate(any(InfoUpdateRequestDTO.class));
+    }
+
+    @Test
+    @DisplayName("내 정보수정 실패")
+    @WithMockUser
+    public void infoUpdateFail() throws Exception {
+
+        given(applicantService.infoUpdate(any(InfoUpdateRequestDTO.class)))
+                .willReturn("fail"); // given : Mock 객체가 특정 상황에서 해야하는 행위를 정의하는 메소드.
+
+        MvcResult mvcResult = mockMvc.perform(put("/applicant/me")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new InfoUpdateRequestDTO())))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        String responseContent = mvcResult.getResponse().getContentAsString();
+        ResponseDTO responseDTO = objectMapper.readValue(responseContent, ResponseDTO.class);
+        assertEquals(401, responseDTO.getStateCode());
+        assertEquals(false, responseDTO.isSuccess());
+        assertEquals("fail", responseDTO.getData());
+
+        verify(applicantService).infoUpdate(any(InfoUpdateRequestDTO.class));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
