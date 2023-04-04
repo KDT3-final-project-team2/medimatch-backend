7205bf6564f5d804a113ff69d5f4cf7d91c76a01
yunhuiGwak <kwakyh17@naver.com>
2023-04-04 13:32:49 +0900

    feat: 약관 repo 생성

---
+++ src/main/java/com/project/finalproject/term/repository/TermRepository.java
@@ -0,0 +1,15 @@
+package com.project.finalproject.term.repository;
+
+import com.project.finalproject.term.entity.Term;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface TermRepository extends JpaRepository<Term, Long> {
+
+    @Query("SELECT t FROM Term t WHERE t.company.id = :companyId")
+    Optional<Term> findByCompanyId(@Param("companyId") Long companyId);
+
+}
