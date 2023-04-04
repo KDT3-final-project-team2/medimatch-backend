7f6e4cf818a2a03996f109a776bbd92e26aba348
Flimberkim <kws1111049@gmail.com>
2023-04-05 01:54:31 +0900

    #17 fix: 기업(Company) 회원가입 관련 수정(ResponseDTO로 반환)

--- src/main/java/com/project/finalproject/signup/controller/CompanySignupController.java
+++ src/main/java/com/project/finalproject/signup/controller/CompanySignupController.java
@@ -1,5 +1,6 @@
 package com.project.finalproject.signup.controller;
 
+import com.project.finalproject.global.dto.ResponseDTO;
 import com.project.finalproject.signup.dto.CompanySignupReqDTO;
 import com.project.finalproject.signup.service.CompanySignupService;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +17,9 @@ public class CompanySignupController {
      * 기업회원 회원가입
      */
     @PostMapping("/company/signup")
-    public String signUp(@RequestBody CompanySignupReqDTO companySignupReqDTO){
+    public ResponseDTO signUp(@RequestBody CompanySignupReqDTO companySignupReqDTO){
         if(companySignupReqDTO.getCompanyEmail() == null || companySignupReqDTO.getCompanyPassword() == null){
-            return "아이디와 비밀번호를 정확히 입력해주세요";
+            return new ResponseDTO(401, false,"INVALID", "이메일과 비밀번호를 정확히 입력해주세요.");
         }
 
         return companySignupService.signUp(companySignupReqDTO);
