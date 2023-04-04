514de28e22640218fc04da9b1e2194bf3c46908b
Flimberkim <kws1111049@gmail.com>
2023-04-04 16:38:03 +0900

    #52 fix: SecurityConfig 수정

--- src/main/java/com/project/finalproject/global/config/SecurityConfig.java
+++ src/main/java/com/project/finalproject/global/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.project.finalproject.global.config;
 
+import com.project.finalproject.global.jwt.utils.JwtExceptionFilter;
 import com.project.finalproject.global.jwt.utils.JwtFilter;
 import com.project.finalproject.global.jwt.utils.JwtProperties;
 import com.project.finalproject.global.jwt.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,14 +19,21 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-    // jwt 토큰을 사용하지 않는 URL
+    /**
+     * 토큰을 사용하지 않는 허용 url
+     */
     String[] permitUrl = {"/**",
             "/company/login","/applicant/signup","applicant/checkemail",
             "/applicant/login","/company/signup", "/refresh","/admin/term/**"};
+
+    private final JwtUtil jwtUtil;
+    private final JwtProperties jwtProperties;
+    private final StringRedisTemplate stringRedisTemplate;
 
 
     @Bean
@@ -36,15 +46,19 @@ public class SecurityConfig {
                 .formLogin().disable() //formLogin 인증 비활성화
                 .httpBasic().disable() //httpBasic 인증 비활성화
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http    .authorizeRequests()
+                .mvcMatchers(permitUrl)
+                .permitAll()
                 .and()
                 .authorizeRequests()
-                .antMatchers(permitUrl)
-                .permitAll()
                 .anyRequest().authenticated();
 
+
         http
-                .addFilterBefore(new JwtFilter(new JwtUtil(new JwtProperties())), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(JwtFilter.of(jwtUtil, jwtProperties, stringRedisTemplate), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(JwtExceptionFilter.of(jwtUtil, jwtProperties), JwtFilter.class);
 
         return http.build();
     }
