f90def8b1476c66a2d08d5eaaac3e3da1aa60dba
yunhuiGwak <kwakyh17@naver.com>
2023-04-05 14:20:12 +0900

    feat: AdminService 생성 - 기업회원 목록조회

---
+++ src/main/java/com/project/finalproject/company/service/AdminService.java
@@ -0,0 +1,14 @@
+package com.project.finalproject.company.service;
+
+import com.project.finalproject.company.dto.AdminCompanyRes;
+import com.project.finalproject.company.entity.enums.CompanyType;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface AdminService {
+
+    // 기업회원 목록조회
+    List<AdminCompanyRes.CompanyListDTO> showCompanyList(CompanyType companyType);
+
+}
