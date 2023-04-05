679e77bc52c1709bb82299846a45a650fd1bf22e
yunhuiGwak <kwakyh17@naver.com>
2023-04-05 17:38:25 +0900

    fix: 개인회원 목록조회 controller 추가

--- src/main/java/com/project/finalproject/company/controller/AdminController.java
+++ src/main/java/com/project/finalproject/company/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.project.finalproject.company.controller;
 
+import com.project.finalproject.company.dto.AdminApplicantRes;
 import com.project.finalproject.company.dto.AdminCompanyRes;
 import com.project.finalproject.company.entity.enums.CompanyType;
 import com.project.finalproject.company.service.AdminService;
@@ -26,4 +27,13 @@ public class AdminController {
 
         return new ResponseDTO<>().ok(companyList, "companyList success");
     }
+
+    @GetMapping("/applicants")
+    public ResponseDTO<?> showApplicantList() {
+        //TODO : 토큰 완성되면 개인회원목록조회 파라미터 수정하기
+        List<AdminApplicantRes.ApplicantListDTO> applicantList = adminService.showApplicantList();
+
+        return new ResponseDTO<>().ok(applicantList, "applicantList success");
+    }
+
 }
