2aaaddc556786b963a1eb9dbaaed295048fd5142
yunhuiGwak <kwakyh17@naver.com>
2023-04-05 14:22:00 +0900

    fix: CompanyRepo에 findCompanyType 추가 -기업회원목록조회용 findCompanyType 추가

--- src/main/java/com/project/finalproject/company/repository/CompanyRepository.java
+++ src/main/java/com/project/finalproject/company/repository/CompanyRepository.java
@@ -1,11 +1,19 @@
 package com.project.finalproject.company.repository;
 
 import com.project.finalproject.company.entity.Company;
+import com.project.finalproject.company.entity.enums.CompanyType;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository< Company, Long > {
 
     Optional<Company> findByEmail(String email);
+
+    @Query("SELECT c FROM Company c WHERE c.companyType = 'COMPANY'")
+    List<Company> findByCompanyType(@Param("companyType") CompanyType companyType);
+
 }
