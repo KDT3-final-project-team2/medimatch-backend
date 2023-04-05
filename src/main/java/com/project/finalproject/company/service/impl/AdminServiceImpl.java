6480ec5fc880804f433fe0ab59d33c97546a89eb
yunhuiGwak <kwakyh17@naver.com>
2023-04-05 17:37:36 +0900

    fix: 개인회원 목록조회 serviceimpl 추가

--- src/main/java/com/project/finalproject/company/service/impl/AdminServiceImpl.java
+++ src/main/java/com/project/finalproject/company/service/impl/AdminServiceImpl.java
@@ -1,14 +1,13 @@
 package com.project.finalproject.company.service.impl;
 
+import com.project.finalproject.applicant.entity.Applicant;
+import com.project.finalproject.applicant.repository.ApplicantRepository;
+import com.project.finalproject.company.dto.AdminApplicantRes;
 import com.project.finalproject.company.dto.AdminCompanyRes;
-import com.project.finalproject.company.entity.Company;
 import com.project.finalproject.company.entity.enums.CompanyType;
-import com.project.finalproject.company.exception.CompanyException;
-import com.project.finalproject.company.exception.CompanyExceptionType;
 import com.project.finalproject.company.repository.CompanyRepository;
 import com.project.finalproject.company.service.AdminService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,11 +19,20 @@ import java.util.stream.Collectors;
 public class AdminServiceImpl implements AdminService {
 
     private final CompanyRepository companyRepository;
+    private final ApplicantRepository applicantRepository;
 
     @Transactional(readOnly = true)
     @Override
     public List<AdminCompanyRes.CompanyListDTO> showCompanyList(CompanyType companyType) {
         return companyRepository.findByCompanyType(companyType).stream()
                 .map(AdminCompanyRes.CompanyListDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<AdminApplicantRes.ApplicantListDTO> showApplicantList() {
+        List<Applicant> applicants = applicantRepository.findAll();
+        return applicants.stream()
+                .map(AdminApplicantRes.ApplicantListDTO::new).collect(Collectors.toList());
     }
 }
