36ef6dfe64d148860149ab0c5a8597b7ff9ec6df
yunhuiGwak <kwakyh17@naver.com>
2023-04-05 17:36:49 +0900

    fix: 개인회원 목록조회 service 추가

--- src/main/java/com/project/finalproject/company/service/AdminService.java
+++ src/main/java/com/project/finalproject/company/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.project.finalproject.company.service;
 
+import com.project.finalproject.company.dto.AdminApplicantRes;
 import com.project.finalproject.company.dto.AdminCompanyRes;
 import com.project.finalproject.company.entity.enums.CompanyType;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,8 @@ public interface AdminService {
 
     // 기업회원 목록조회
     List<AdminCompanyRes.CompanyListDTO> showCompanyList(CompanyType companyType);
+
+    //개인회원 목록조회
+    List<AdminApplicantRes.ApplicantListDTO> showApplicantList();
 
 }
