807b3aa159e764d5d983899caea59a0c59b46d63
yunhuiGwak <kwakyh17@naver.com>
2023-04-08 22:24:30 +0900

    fix: @NotNull 설정추가

--- src/main/java/com/project/finalproject/term/dto/TermFormDTO.java
+++ src/main/java/com/project/finalproject/term/dto/TermFormDTO.java
@@ -6,6 +6,7 @@ import com.project.finalproject.term.entity.enums.TermType;
 import lombok.*;
 
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 public class TermFormDTO {
     @Getter
@@ -13,14 +14,18 @@ public class TermFormDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class registerDTO{
+        @NotNull(message = "약관내용이 전달되지 않았습니다.")
         private String content; // 약관내용
+        @NotNull(message = "약관타입이 전달되지 않았습니다.")
         private TermType type;  // 약관 타입
+        @NotNull(message = "약관버전이 전달되지 않았습니다.")
         private String version; // 약관 버전
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         private LocalDateTime createDate;   //약관등록일
         private LocalDateTime editDate;     //약관수정일
 
+        @NotNull(message = "약관상태가 전달되지 않았습니다.")
         private TermStatus status;  //약관상태(사용,임시저장,폐기)
     }
 
