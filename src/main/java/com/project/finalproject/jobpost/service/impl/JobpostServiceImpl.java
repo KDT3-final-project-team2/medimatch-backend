1b1afbd03f262315a257c55607a9dcad82da4fc4
yunhuiGwak <kwakyh17@naver.com>
2023-04-08 19:48:02 +0900

    feat: 채용공고 삭제 serviceimpe 수정

--- src/main/java/com/project/finalproject/jobpost/service/impl/JobpostServiceImpl.java
+++ src/main/java/com/project/finalproject/jobpost/service/impl/JobpostServiceImpl.java
@@ -1,9 +1,12 @@
 package com.project.finalproject.jobpost.service.impl;
 
+import com.project.finalproject.jobpost.entity.Jobpost;
 import com.project.finalproject.jobpost.repository.JobpostRepository;
 import com.project.finalproject.jobpost.service.JobpostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +15,18 @@ public class JobpostServiceImpl implements JobpostService {
     private final JobpostRepository jobpostRepository;
 
     @Override
-    public String deleteJobpost(Long jobpostId) {
+    public String remove(Long jobpostId) {
+
+        Jobpost jobpost = jobpostRepository.findById(jobpostId).orElse(null);
+
+        String filePath = jobpost.getFilepath();
 
         jobpostRepository.deleteById(jobpostId);
+
+        File post = new File(filePath);
+
+        post.delete();
+
         return "success";
     }
 }
