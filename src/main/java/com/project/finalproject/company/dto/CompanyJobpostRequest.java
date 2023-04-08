b8e34e7f06e3b60786d40b40c115fbc53823df12
yunhuiGwak <kwakyh17@naver.com>
2023-04-08 22:05:36 +0900

    fix: @NotNull 설정

--- src/main/java/com/project/finalproject/company/dto/CompanyJobpostRequest.java
+++ src/main/java/com/project/finalproject/company/dto/CompanyJobpostRequest.java
@@ -1,13 +1,11 @@
 package com.project.finalproject.company.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.finalproject.applicant.entity.enums.Sector;
 import com.project.finalproject.jobpost.entity.enums.JobpostEducation;
 import com.project.finalproject.jobpost.entity.enums.JobpostStatus;
 import com.project.finalproject.jobpost.entity.enums.JobpostWorkExperience;
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
-
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 /**
@@ -23,12 +21,22 @@ public class CompanyJobpostRequest {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class CreateDTO{
+
+        @NotNull
         private String title; //공고 제목
+
         private LocalDateTime startDate; //공고 시작 날짜
         private LocalDateTime dueDate; //공고 종료 날짜
+
+        @NotNull(message = "직무가 전달되지 않았습니다.")
         private Sector sector; //모집 직무
+
+        @NotNull(message = "학력이 전달되지 않았습니다.")
         private JobpostEducation education; // 학력
+
+        @NotNull(message = "경력이 전달되지 않았습니다.")
         private JobpostWorkExperience workExperience; // 경력
+
         private Integer recruitNum; // 모집 인원
         private String filePath; // 채용 공고 파일 (== 내용)
 
